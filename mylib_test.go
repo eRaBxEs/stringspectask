9078c0524a710577c1d08f720b642521646b9b6b
eRaBxEs <ehiojee@yahoo.com>
2022-03-24 13:23:07 +0100

    add averageNumber test

--- mylib_test.go
+++ mylib_test.go
@@ -25,3 +25,25 @@ func TestTestValidity(t *testing.T) {
 		}
 	}
 }
+
+func TestAverageNumber(t *testing.T) {
+	myTest := []struct {
+		inputName      string
+		expectedOutput int
+		expectedError  error
+	}{
+		{inputName: "23-ab-48-caba-56-haha", expectedOutput: 5, expectedError: nil},
+	}
+
+	for _, tt := range myTest {
+
+		actualOutput, actualError := averageNumber(tt.inputName)
+
+		if actualError != tt.expectedError {
+			t.Error("should not get an error")
+		}
+		if actualOutput != tt.expectedOutput {
+			t.Errorf("got %v, want %v", actualOutput, tt.expectedOutput)
+		}
+	}
+}
